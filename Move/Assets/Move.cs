9d079f1b08972616eaa826fb4f175a9e2c1050dc
frostyday <frostyday@naver.com>
2020-09-25 17:33:56 +0900

    7강

---
+++ Move/Assets/Move.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Vector3 target = new Vector3(-5,1,0);
+    void Update()
+    {
+        //1.MoveTowards
+       // transform.position =
+            //Vector3.MoveTowards(transform.position,target, 1f);
+
+        //2.SmoothDamp
+        //Vector3 velo = Vector3.zero;
+
+        //transform.position = 
+           // Vector3.SmoothDamp(transform.position,target,ref velo, 0.1f);
+
+           //3.Lerp
+           //transform.position = 
+            // Vector3.Lerp(transform.position,target, 0.005f);
+        //4.SLerp(구면 선형 보간)  
+        transform.position = 
+             Vector3.Slerp(transform.position,target, 0.005f);  
+    }
+
+}
